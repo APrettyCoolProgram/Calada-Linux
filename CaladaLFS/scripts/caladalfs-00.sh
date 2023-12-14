448bc79cc43933621139d754b6fd22a2ea2a421b
APrettyCoolProgram <github@aprettycoolprogram.com>
2023-12-14 18:35:06 -0500

    Update

--- CaladaLFS/scripts/caladalfs-00.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## CaladaLFS-00.sh
-## b231214.1822
-## Creates required CaladaLFS framework components.
-
-mkdir -p ~/.CaladaLFS/{Logs,Scripts,Temp}
-
-mv ~/caladalfs-00.sh ~/.CaladaLFS/Scripts/
--- CaladaLFS/scripts/caladalfs-02.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## CaladaLFS Script #01
-## 
-## Installs packages required by LFS.
-
-sudo apt-get install build-essential bison gawk texinfo
