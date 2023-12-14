6f4100904b8f07e7886ab064381822f49c07cc9f
APrettyCoolProgram <github@aprettycoolprogram.com>
2023-12-14 18:36:09 -0500

    b231214

---
+++ CaladaLFS/scripts/CaladaLFS-00.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## CaladaLFS-00.sh
+## b231214.1822
+## Creates required CaladaLFS framework components.
+
+mkdir -p ~/.CaladaLFS/{Logs,Scripts,Temp}
+
+mv ~/CaladaLFS-00.sh ~/.CaladaLFS/Scripts/
